6ca543854f39bbd73088d02f3e8bee688832d01a
Maker39 <47417361+Maker39@users.noreply.github.com>
2026-09-01 01:40:02 +0200

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Название целевой платы
+# Название вашей целевой платы
 BOARD_NAME="WT3020H16M"
 BOARD_DIR="padavan-ng/trunk/configs/boards/NEXX/${BOARD_NAME}"
 
-echo "=== 1. Перезапись пользовательского board.h ==="
+echo "=== 1. Формирование чистого board.h под Kimax BS-U35-WF ==="
 cat << 'EOF' > ${BOARD_DIR}/board.h
 /* Blueendless Kimax BS-U35-WF (WT3020H16M Mod) */
 #define BOARD_PID		"WT3020H16M"
@@ -16,13 +16,15 @@ cat << 'EOF' > ${BOARD_DIR}/board.h
 #define BOARD_BOOT_TIME		25
 #define BOARD_FLASH_TIME	120
 
-#define BOARD_GPIO_BTN_RESET	13
+/* Принудительное переопределение GPIO аппаратного уровня */
+#define BOARD_GPIO_BTN_RESET	13  /* Сброс на GPIO 13 */
 #undef  BOARD_GPIO_BTN_WPS
 
-#define BOARD_GPIO_LED_WIFI	7
-#define BOARD_GPIO_LED_POWER	14
-#define BOARD_GPIO_LED_SATA	15
+#define BOARD_GPIO_LED_WIFI	7   /* Синий WiFi */
+#define BOARD_GPIO_LED_POWER	14  /* Зеленый LAN */
+#define BOARD_GPIO_LED_SATA	15  /* Красный/Оранжевый HDD */
 
+/* Инверсия сигналов */
 #define BOARD_GPIO_LED_INVERTED
 #define BOARD_GPIO_BTN_INVERTED
 
@@ -34,86 +36,16 @@ cat << 'EOF' > ${BOARD_DIR}/board.h
 #define BOARD_HAS_EPHY_WND	0
 #define BOARD_NUM_UPHY_LEDS	0
 #define BOARD_USB_PORT_COUNT	1
+
+/* СИСТЕМНЫЙ ХАК: Принудительное освобождение GPIO 14 и 15 из регистров MT7620А */
+/* Перенаправляем функции драйвера board_init на инициализацию регистров pinmux */
+#define BOARD_INIT_CUSTOM                                                 \
+    do {                                                                  \
+        /* Чтение и модификация регистра PMX_EPHY_LED_AN (offset 0x60) */ \
+        /* Переводим пины EPHY (14 и 15) в режим GPIO, отключая LED LAN */\
+        *((volatile uint32_t *)(0xb0000060)) &= ~(0x1F << 15);            \
+    } while (0)
+
 EOF
 
-echo "=== 2. Корректировка дерева устройств DTS ядра ==="
-
-# Находим точный файл DTS для WT3020 в ядре Linux
-DTS_FILE=$(find padavan-ng/trunk/linux -name "WT3020*.dts" | head -n 1)
-
-if [ -f "$DTS_FILE" ]; then
-    echo "DTS файл найден: $DTS_FILE. Переписываем структуру пинов."
-    
-    cat << 'EOF' > "$DTS_FILE"
-/dts-v1/;
-
-/include/ "mt7620a.dtsi"
-
-/ {
-	compatible = "nexx,wt3020", "ralink,mt7620a-soc";
-	model = "Blueendless Kimax BS-U35-WF";
-
-	gpio-leds {
-		compatible = "gpio-leds";
-
-		wifi {
-			label = "wt3020:blue:wifi";
-			gpios = <&gpio0 7 1>;
-		};
-
-		power {
-			label = "wt3020:blue:power";
-			gpios = <&gpio0 14 1>;
-		};
-	};
-
-	gpio-keys-polled {
-		compatible = "gpio-keys-polled";
-		#address-cells = <1>;
-		#size-cells = <0>;
-		poll-interval = <20>;
-
-		reset {
-			label = "reset";
-			gpios = <&gpio0 13 1>;
-			linux,code = <0x102>;
-		};
-	};
-};
-
-&gpio0 {
-	status = "okay";
-};
-
-&pinctrl {
-	state_default: pinctrl0 {
-		gpio {
-			ralink,group = "ephy", "wled";
-			ralink,function = "gpio";
-		};
-	};
-};
-
-&ethernet {
-	mtd-mac-address = <&factory 0x4>;
-	ralink,port-map = "llllw";
-};
-
-&wmac {
-	ralink,mtd-eeprom = <&factory 0>;
-};
-
-&ehci {
-	status = "okay";
-};
-
-&ohci {
-	status = "okay";
-};
-EOF
-    echo "DTS успешно заменен."
-else
-    echo "ОШИБКА: DTS файл не найден в структуре директорий!"
-fi
-
-echo "=== Скрипт подготовки выполнен без использования опасных sed ==="
+echo "=== Скрипт подготовки успешно завершен! ==="
