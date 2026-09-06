e6d0f3bf1e99e3c68cea35c8777b098d991da77e
Maker39 <47417361+Maker39@users.noreply.github.com>
2026-09-06 13:52:49 +0200

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BOARD_H=$(find . -name "board.h" | grep -i "WT3020" | grep -v "uboot" | head -n 1)
 KERNEL_CONFIG=$(find . -name "kernel-3.4.x.config" | grep -i "WT3020" | head -n 1)
-
+echo "=== Скрипт pre_build.sh: Настройка
 if [ -n "$BOARD_H" ] && [ -f "$BOARD_H" ]; then
     sed -i 's/#define BOARD_NUM_ETH_EPHY.*/#define BOARD_NUM_ETH_EPHY     1/g' "$BOARD_H"
     sed -i 's/#define BOARD_HAS_5P.*/\/\/#define BOARD_HAS_5P/g' "$BOARD_H"
