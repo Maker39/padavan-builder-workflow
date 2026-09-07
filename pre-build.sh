271e7851a4d0c873dcb5e7239a608295ded0fa48
Maker39 <47417361+Maker39@users.noreply.github.com>
2026-09-07 21:43:37 +0200

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Находим путь к файлу board.h для выбранного профиля платы (например, WT3020)
-# В зависимости от базового конфига, замените WT3020 на имя вашей целевой платы, если выбрали другую
+# Указываем правильный путь к базовой плате WT3020 в исходниках Padavan
 BOARD_H_PATH="trunk/user/shared/boards/WT3020/board.h"
 BOARD_C_PATH="trunk/user/shared/boards/WT3020/board.c"
 
-# 1. Меняем GPIO светодиода Ethernet на 44
+# 1. Меняем GPIO светодиода Ethernet на 44 и ставим инверсию (ACTIVE_LOW)
 sed -i 's/#define BOARD_GPIO_LED_ETH.*/#define BOARD_GPIO_LED_ETH          44/' $BOARD_H_PATH
 sed -i 's/#define BOARD_GPIO_LED_ETH_INV.*/#define BOARD_GPIO_LED_ETH_INV      1/' $BOARD_H_PATH
 
-# 2. Меняем GPIO кнопки Reset на 13
+# 2. Меняем GPIO кнопки Reset на 13 и ставим инверсию
 sed -i 's/#define BOARD_GPIO_BTN_RESET.*/#define BOARD_GPIO_BTN_RESET        13/' $BOARD_H_PATH
 sed -i 's/#define BOARD_GPIO_BTN_RESET_INV.*/#define BOARD_GPIO_BTN_RESET_INV    1/' $BOARD_H_PATH
 
-# 3. Добавляем освобождение банка пинов (PAD_MODE_NAND -> GPIO) в board.c
-# Вставляем команду инициализации сразу после открытия главной функции board_init
-sed -i '/void board_init(void)/!b;n;a\    mips_sys_set_padmode(PAD_MODE_NAND, PAD_MODE_GPIO);' $BOARD_C_PATH
+# 3. Вставляем инициализацию PAD_MODE_NAND в board.c, чтобы процессор освободил GPIO 44
+# Мы добавим её сразу после открытия функции board_init(void)
+sed -i '/void board_init(void)/a \ \ \ \ mips_sys_set_padmode(PAD_MODE_NAND, PAD_MODE_GPIO);' $BOARD_C_PATH
