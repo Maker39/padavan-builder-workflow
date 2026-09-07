ad52f69f676b66cc190b4b19e2868d9c00c43ad2
Maker39 <47417361+Maker39@users.noreply.github.com>
2026-09-07 21:26:46 +0200

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 # Путь к папке вашей платы в исходниках Padavan (замените WT3020 на имя вашего профиля, если оно другое)
-#BOARD_DIR="configs/boards/NEXX/WT3020H16M/board.h"
-TARGET_H="configs/boards/NEXX/WT3020H16M"
-#TARGET_C="configs/boards/NEXX/WT3020H16M"
+BOARD_DIR="configs/boards/NEXX/WT3020H16M"
 
-echo " 1. Меняем GPIO светодиода Ethernet на 44 в board.h"
-sed -i 's/#define BOARD_GPIO_LED_ETH.*/#define BOARD_GPIO_LED_ETH          44/' "$TARGET_H/board.h"
-sed -i 's/#define BOARD_GPIO_LED_ETH_INV.*/#define BOARD_GPIO_LED_ETH_INV      1/' "$TARGET_H/board.h"
+# 1. Меняем GPIO светодиода Ethernet на 44 в board.h
+sed -i 's/#define BOARD_GPIO_LED_ETH.*/#define BOARD_GPIO_LED_ETH          44/' "$BOARD_DIR/board.h"
+sed -i 's/#define BOARD_GPIO_LED_ETH_INV.*/#define BOARD_GPIO_LED_ETH_INV      1/' "$BOARD_DIR/board.h"
 
-echo " 2. Меняем GPIO кнопки Reset на 13 в board.h"
-sed -i 's/#define BOARD_GPIO_BTN_RESET.*/#define BOARD_GPIO_BTN_RESET        13/' "$TARGET_H/board.h"
-sed -i 's/#define BOARD_GPIO_BTN_RESET_INV.*/#define BOARD_GPIO_BTN_RESET_INV    1/' "$TARGET_H/board.h"
+# 2. Меняем GPIO кнопки Reset на 13 в board.h
+sed -i 's/#define BOARD_GPIO_BTN_RESET.*/#define BOARD_GPIO_BTN_RESET        13/' "$BOARD_DIR/board.h"
+sed -i 's/#define BOARD_GPIO_BTN_RESET_INV.*/#define BOARD_GPIO_BTN_RESET_INV    1/' "$BOARD_DIR/board.h"
 
-echo " 3. Вставляем инициализацию PAD_MODE_NAND в начало функции board_init в board.c"
+# 3. Вставляем инициализацию PAD_MODE_NAND в начало функции board_init в board.c
 # Это освободит банк gpio2 для работы светодиода на SPI Flash устройствах
-sed -i '/void board_init(void)/!b;n;a\    mips_sys_set_padmode(PAD_MODE_NAND, PAD_MODE_GPIO);' "$TARGET_H/board.c"
+sed -i '/void board_init(void)/!b;n;a\    mips_sys_set_padmode(PAD_MODE_NAND, PAD_MODE_GPIO);' "$BOARD_DIR/board.c"
